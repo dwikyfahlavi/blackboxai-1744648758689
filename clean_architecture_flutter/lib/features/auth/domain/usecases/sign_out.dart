3ee23d39cbe7af031c51d875048e6202d99fcaed
Dwiqy Fahlavi Muhammad Ramdani <65699938+dwikyfahlavi@users.noreply.github.com>
2025-04-14 23:39:57 +0700

    Add clean_architecture_flutter/lib/features/auth/domain/usecases/sign_out.dart 1744648796086

---
+++ clean_architecture_flutter/lib/features/auth/domain/usecases/sign_out.dart
@@ -0,0 +1,14 @@
+import 'package:fpdart/fpdart.dart';
+import '../../../../core/usecases/usecase.dart';
+import '../repositories/auth_repository.dart';
+
+class SignOut implements UseCase<void, NoParams> {
+  final AuthRepository repository;
+
+  SignOut(this.repository);
+
+  @override
+  Future<Either<Failure, void>> call(NoParams params) async {
+    return await repository.signOut();
+  }
+}
