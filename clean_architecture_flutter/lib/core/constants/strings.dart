40854b5753ecc9920634abb69ae15b236694fe29
Dwiqy Fahlavi Muhammad Ramdani <65699938+dwikyfahlavi@users.noreply.github.com>
2025-04-14 23:39:50 +0700

    Add clean_architecture_flutter/lib/core/constants/strings.dart 1744648789681

---
+++ clean_architecture_flutter/lib/core/constants/strings.dart
@@ -0,0 +1,7 @@
+class AppStrings {
+  static const String appName = 'Clean Architecture Flutter';
+  static const String noRouteFound = 'No Route Found';
+  static const String serverFailure = 'Server Failure';
+  static const String cacheFailure = 'Cache Failure';
+  static const String unexpectedError = 'Unexpected Error';
+}
