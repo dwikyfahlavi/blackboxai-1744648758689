a40cc0595063c4b9fd921b511cf3da1f1078541c
Dwiqy Fahlavi Muhammad Ramdani <65699938+dwikyfahlavi@users.noreply.github.com>
2025-04-14 23:40:02 +0700

    Add clean_architecture_flutter/lib/features/auth/presentation/cubit/auth_state.dart 1744648801010

---
+++ clean_architecture_flutter/lib/features/auth/presentation/cubit/auth_state.dart
@@ -0,0 +1,34 @@
+import 'package:equatable/equatable.dart';
+import '../../domain/entities/user.dart';
+
+abstract class AuthState extends Equatable {
+  const AuthState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class AuthInitial extends AuthState {}
+
+class AuthLoading extends AuthState {}
+
+class AuthAuthenticated extends AuthState {
+  final User user;
+  final String role;
+
+  const AuthAuthenticated(this.user) : role = user.role!;
+
+  @override
+  List<Object> get props => [user, role];
+}
+
+class AuthUnauthenticated extends AuthState {}
+
+class AuthError extends AuthState {
+  final String message;
+
+  const AuthError(this.message);
+
+  @override
+  List<Object> get props => [message];
+}
